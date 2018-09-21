1a499d1de20fdf539a6cf69ee5b9aff4b345484a
Romain Fournier <rmn.fournier@gmail.com>
2018-09-21 13:29:13 +0200

    Tensorflow
    Prepare  tensorflow for Repository

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# ACANN
+Solving an Analytic Continuation problem with a deep Artificial Neural Network (ANN)
+
+#1. Problem Description
+ We are interested in the analytic continuation of the imaginary Green's function into the real frequency domain. We use a supervised learning approach for efficiently solving this problem. This method has both advantages of regularizing the problem by distilling prior knowledge into the training dataset and being robust against noisy inputs. Interested readers can look at our paper to get further details.
+
+# 2. Getting Started 
+ We propose two different implementations of our model. One using PyTorch and the other one TensorFlow, as well as a Matlab file that allows quickly building a dataset. 
+
+## 2.1 Build the dataset
+ Getting the imaginary Green's function from the electron density function is a stable process, which allows generating a dataset of training examples by a simple two steps process. 
+1. Generate a set of spectral density functions that look like the problem to handle (e.g., spectral density functions containing a quasiparticle peak).
+2. Convert them into Green's function in imaginary time. For performance increase, it is better to save the Legendre coefficients of this function instead of the whole function.
+ 
+The folder Generate Data offers a Matlab implementation of this process, used in the previously mentioned paper. 
+
+## 2.2 Train the model
+ We offer two ways of training the  Deep Neural Network: TensorFlow and Pytorch.
+###2.2.1 TensorFlow
+ The Folder TensorFlow contains the necessary files to train the neural network. It requires Keras to be installed on the machine.  Taping the following commands launches the training :
+
+  'python train_ACANN.py data_location nb_data name nb_layers nb_epochs units' ,
+  
+where 'data_location' is the path to the folder containing the files (A_data.csv, Gl_data.csv, A_validationset.csv, Gl_validationset.csv), 
+
+'nb_data' is the number of data to consider in the training dataset, 
+
+'name' is the prefix of the output files, 
+
+'nb_layers' is the number of dense layers to use, 
+
+'nb_epochs' is the number of epochs to train on, 
+
+and 'units' is the number of weights in the different layers. 
+
+For example : python train_ACANN.py ./Dataset 25000 test 3 100 1024 1024
+
+###2.2.2 PyTorch
+ The Folder PyTorch contains the necessary files to train the neural network.
+## 2.3 Make predictions
+### 2.3.1 TensorFlow
+### 2.3.2 PyTorch
+
+## 2.4 Examples 
--- TensorFlow/ACANN_train.py
+++ TensorFlow/ACANN_train.py
@@ -11,14 +11,11 @@ import random
 import sys
 from keras.layers import Dense, Activation, Dropout, BatchNormalization
 from keras.optimizers import Adam
+from keras.callbacks import History
 import numpy as np
-import csv
 import pandas
-from sklearn import metrics
-import tensorflow as tf
-from keras.callbacks import History,ReduceLROnPlateau
-from keras.models import load_model
-from keras.callbacks import TensorBoard
+
+#from keras.callbacks import TensorBoard
 ###### Database
 #Training files
 filename_A = "../Database/A_data.csv"
@@ -28,16 +25,23 @@ filename_Gval = "../Database/nl_validationset.csv"
 
 
 ####### Input arguments
- ## 1st argument is the number of data to consider in the file
-database_size=int(sys.argv[1])
- ## 2nd argument is the name used as prefix for the saved files
-name=sys.argv[2]
- ## 3rd argument is the number of layers to use
-nb_layers=int(sys.argv[3])
-## 3th argument is the number of epochs
-nb_epochs=int(sys.argv[4])
+## Find the dataset
+prefix = str(sys.argv[1])
+filename_A = prefix+"/A_data.csv"
+filename_G = prefix+"/nl_data.csv"
+filename_Aval = prefix+"/A_validationset.csv"
+filename_Gval = prefix+"/nl_validationset.csv"
 
-## argument 5 -> 5+nb_layers-1 correspond to the number of hidden units
+ ## 2nd argument is the number of data to consider in the file
+database_size=int(sys.argv[2])
+ ## 3rd argument is the name used as prefix for the saved files
+name=sys.argv[3]
+ ## 4th argument is the number of layers to use
+nb_layers=int(sys.argv[4])
+## 5th argument is the number of epochs
+nb_epochs=int(sys.argv[5])
+
+## argument 6 -> 6+nb_layers-1 correspond to the number of hidden units
 
 
 ## Variable to save the intermediate results
@@ -47,25 +51,23 @@ history = History()
 adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
 model = Sequential()
-model.add(Dense(units=int(sys.argv[5]),input_dim=64 ))
+model.add(Dense(units=int(sys.argv[6]),input_dim=64 ))
 for ii in range(1,nb_layers):
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(0.02))
-    model.add(Dense(units=int(sys.argv[5+ii])))
+    model.add(Dense(units=int(sys.argv[6+ii])))
 model.add(Activation('softmax'))
 print("The model has been initiated with Sequential.")
 
 ########## Activate TensorBord in order to observe the evolution of the training live
-tbCallBack = TensorBoard(log_dir='./TensorBoard/'+str(name), histogram_freq=0, write_graph=True, write_images=True)
-########## Reduce the learning rate when the validation does not improve over the 5 last epochs
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.9,
-                              patience=5, min_lr=0.0)
+#tbCallBack = TensorBoard(log_dir='./TensorBoard/'+str(name), histogram_freq=0, write_graph=True, write_images=True)
+
 
 ############ Read normalize and prepare the data (training + validation)
 n = sum(1 for line in open(filename_A))  #number of records in file (excludes header)
 s = database_size; #desired sample size
-skip = sorted(random.sample(xrange(0,n),n-1-s)) #the 0-indexed header will not be included in the skip list
+skip = sorted(random.sample(range(0,n),n-1-s)) #range -> xrange if python 2
 df_A = pandas.read_csv(filename_A, skiprows=skip)
 df_G = pandas.read_csv(filename_G, skiprows=skip)
 x_train=df_G.as_matrix()
@@ -76,24 +78,22 @@ df_Aval = pandas.read_csv(filename_Aval)
 df_Gval = pandas.read_csv(filename_Gval)
 x_val=df_Gval.as_matrix()
 y_val=df_Aval.as_matrix()
-print(np.mean(y_val.sum(axis=1, keepdims=True)))
 y_val=y_val/y_val.sum(axis=1, keepdims=True)
 
 ######### Compile the model
 model.compile(loss='kullback_leibler_divergence',
         optimizer=adam,
         metrics=['mae'])
-history=model.fit(x_train, y_train, epochs=nb_epochs, initial_epoch=0,batch_size=100,validation_data=(x_val,y_val), callbacks=[tbCallBack,reduce_lr])
+history=model.fit(x_train, y_train, epochs=nb_epochs, initial_epoch=0,batch_size=25000,validation_data=(x_val,y_val)) #, callbacks=[tbCallBack]
 
 
 ######## Save the results
-print(history.history.keys())
-f_handle = file('Results/'+str(name)+'/mae.csv', 'a')
+f_handle = open(str(name)+'_mae.csv', 'a')
 np.savetxt(f_handle, history.history['mean_absolute_error'])
 f_handle.close()
-f_handle = file('Results/'+str(name)+'/val_mae.csv', 'a')
+f_handle = open(str(name)+'_val_mae.csv', 'a')
 np.savetxt(f_handle, history.history['val_mean_absolute_error'])
 f_handle.close()
 
-model.save('Results/'+str(name)+'/model.h5')
-
+model.save(str(name)+'_model.h5')
+model.save_weights(str(name)+'_weights.h5')
